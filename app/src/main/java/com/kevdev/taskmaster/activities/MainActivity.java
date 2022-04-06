5a0c102d9069703eb6e2ebd358875cc3c7b97e44
KevLaMDev <lamarca.k@gmail.com>
2022-04-05 21:08:58 -0400

    lab 33 complete

--- app/src/main/java/com/kevdev/taskmaster/activities/MainActivity.java
+++ app/src/main/java/com/kevdev/taskmaster/activities/MainActivity.java
@@ -14,7 +14,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.amplifyframework.api.graphql.model.ModelMutation;
 import com.amplifyframework.api.graphql.model.ModelQuery;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.Task;
@@ -22,7 +21,10 @@ import com.kevdev.taskmaster.R;
 import com.kevdev.taskmaster.adapters.MainActivityRecyclerViewAdapter;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -31,29 +33,79 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
     SharedPreferences preferences;
     MainActivityRecyclerViewAdapter adapter;
-    List<Task> tasks = new ArrayList<>();
-
+    CompletableFuture<List<Task>> taskCompletableFuture = null;
+    List<Task> dbTasks = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        // Get component ID
-        ImageButton settingsButton = (ImageButton) findViewById(R.id.settingsNavButton);
-        // DB proof of life:
-//        com.amplifyframework.datastore.generated.model.Task testTask =
-//                com.amplifyframework.datastore.generated.model.Task.builder()
-//                    .title("test task")
-//                    .body("test body")
-//                    .taskState(com.amplifyframework.datastore.generated.model.State.NEW)
-//                    .build();
-//        Amplify.API.mutate(ModelMutation.create(testTask),
+        taskCompletableFuture = new CompletableFuture<>();
+        String teamName = preferences.getString(Settings.TEAM_TAG, "No team name");
+        Amplify.API.query(
+                ModelQuery.list(Task.class),
+                success -> {
+                    Log.i(TAG, "successful READ of tasks from DB");
+                    if (success.getData() != null) {
+                        for (Task dbTask : success.getData()) {
+                            if (dbTask.getTeam().getName().equals(teamName)) dbTasks.add(dbTask);
+                        }
+                        taskCompletableFuture.complete(dbTasks);
+                        runOnUiThread(() ->
+                        {
+                            adapter.notifyDataSetChanged();
+                        });
+                    }
+                },
+                failure -> {
+                    taskCompletableFuture.complete(null);
+                    Log.i(TAG, "failure to READ tasks from DB");
+                }
+        );
+//      DB proof of life:
+
+//        Team team1 =
+//                Team.builder().name("Team 1").build();
+//        Task task1 =
+//                Task.builder().title("task1").body("body").team(team1).build();
+//        Amplify.API.mutate(
+//                ModelMutation.create(task1),
+//                successResponse -> Log.i(TAG, "MainActivity.onCreate(): made a task successfully"),
+//                    failureResponse -> Log.i(TAG, "MainActivity.onCreate(): failed with " + failureResponse)
+//                );
+//
+//        Amplify.API.mutate(ModelMutation.create(team1),
 //                    successResponse -> Log.i(TAG, "MainActivity.onCreate(): made a task successfully"),
 //                    failureResponse -> Log.i(TAG, "MainActivity.onCreate(): failed with " + failureResponse)
 //                );
-        // Set OnClickListener
+//        Team team2 =
+//                Team.builder().name("Team 2").build();
+//        Amplify.API.mutate(ModelMutation.create(team2),
+//                successResponse -> Log.i(TAG, "MainActivity.onCreate(): made a task successfully"),
+//                failureResponse -> Log.i(TAG, "MainActivity.onCreate(): failed with " + failureResponse)
+//        );
+//        Team team3 =
+//                Team.builder().name("Team 3").build();
+//        Amplify.API.mutate(ModelMutation.create(team3),
+//                successResponse -> Log.i(TAG, "MainActivity.onCreate(): made a task successfully"),
+//                failureResponse -> Log.i(TAG, "MainActivity.onCreate(): failed with " + failureResponse)
+
+//        );
+
+        setUpSettingsButtonActivity();
+        try {
+            setUpMainActivityRecyclerView();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setUpAddTaskActivity();
+    }
+
+    private void setUpSettingsButtonActivity() {
+        ImageButton settingsButton = (ImageButton) findViewById(R.id.settingsNavButton);
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,9 +113,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(goToTaskDetail);
             }
         });
-
-        setUpMainActivityRecyclerView();
-        setUpAddTaskActivity();
     }
 
     private void setUpAddTaskActivity() {
@@ -82,36 +131,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        String username = preferences.getString(Settings.USERNAME_TAG, "");
         TextView mainPageHeader = findViewById(R.id.mainPageHeader);
-        List<Task> dbTasks = new ArrayList<>();
+        dbTasks.clear();
+        String teamName = preferences.getString(Settings.TEAM_TAG, "No team name");
         Amplify.API.query(
                 ModelQuery.list(Task.class),
                 success -> {
                     Log.i(TAG, "successful READ of tasks from DB");
-                    for (Task dbTask : success.getData()) {
-                        dbTasks.add(dbTask);
+                    if (success.getData() != null) {
+                        for (Task dbTask : success.getData()) {
+                            if (dbTask.getTeam().getName().equals(teamName)) dbTasks.add(dbTask);
+                        }
+                        taskCompletableFuture.complete(dbTasks);
+                        runOnUiThread(() ->
+                        {
+                            adapter.notifyDataSetChanged();
+                        });
                     }
-                    tasks = dbTasks;
-                    runOnUiThread(() ->
-                    {
-                        adapter.notifyDataSetChanged();
-                    });
                 },
-                failure -> Log.i(TAG, "failure to READ tasks from DB")
+                failure -> {
+                    taskCompletableFuture.complete(null);
+                    Log.i(TAG, "failure to READ tasks from DB");
+                }
         );
-        setUpMainActivityRecyclerView();
-        mainPageHeader.setText(username + "\'s tasks");
+        try {
+            setUpMainActivityRecyclerView();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        mainPageHeader.setText(teamName + "\'s tasks");
     }
 
-    private void setUpMainActivityRecyclerView() {
+    private void setUpMainActivityRecyclerView() throws ExecutionException, InterruptedException {
         // Step 1A: Get recycler by Id
         RecyclerView mainActivityRecyclerView = findViewById(R.id.mainPageRecycleView);
         // Step 1B: Set layout manager of RecyclerView
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         mainActivityRecyclerView.setLayoutManager(layoutManager);
-        // Step 1C: Create adapter class and import as global instance var
-        adapter = new MainActivityRecyclerViewAdapter(tasks, this);
+        List<Task> taskList = taskCompletableFuture.get();
+        String teamName = preferences.getString(Settings.TEAM_TAG, "No team name");
+        adapter = new MainActivityRecyclerViewAdapter(taskList, this);
         // set adapter on recyclerView UI element
         mainActivityRecyclerView.setAdapter(adapter);
     }
--- app/src/main/java/com/kevdev/taskmaster/activities/Settings.java
+++ app/src/main/java/com/kevdev/taskmaster/activities/Settings.java
@@ -5,15 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
+import com.amplifyframework.api.graphql.model.ModelQuery;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Team;
 import com.kevdev.taskmaster.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Settings extends AppCompatActivity {
     SharedPreferences preferences;
-    public static final String USERNAME_TAG = "username";
+    public static final String TEAM_TAG = "team";
+    public static final String TAG = "Settings Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +32,43 @@ public class Settings extends AppCompatActivity {
 
         // get preference manager
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String username = preferences.getString(USERNAME_TAG, "");
+        String teamName = preferences.getString(TEAM_TAG, "");
 
-        if (!username.isEmpty()) {
-            EditText usernameEditTest = (EditText) findViewById(R.id.textEditUsername);
-            usernameEditTest.setText(username);
-        }
+//        if (!teamName.isEmpty()) {
+//            EditText usernameEditTest = (EditText) findViewById(R.id.textEditUsername);
+//            usernameEditTest.setText(teamName);
+//        }
+
+        Spinner teamNameSpinner = findViewById(R.id.teamNameSpinnerSettings);
+        List<String> dbTeamNames = new ArrayList<>();
+        Amplify.API.query(
+                ModelQuery.list(Team.class),
+                success -> {
+                    Log.i(TAG, "successful READ of teams from DB");
+                    if (success.getData() != null) {
+                        for (Team team : success.getData()) {
+                            dbTeamNames.add(team.getName());
+                        }
+                        runOnUiThread(() ->
+                        {
+                            teamNameSpinner.setAdapter(new ArrayAdapter<>(
+                                    this,
+                                    android.R.layout.simple_spinner_item,
+                                    dbTeamNames
+                            ));
+                        });
+                    }
+                },
+                failure -> Log.i(TAG, "failure to READ tasks from DB")
+        );
+
         Button saveButton = findViewById(R.id.usernameSubmitButton);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SharedPreferences.Editor preferencesEditor = preferences.edit();
-                EditText usernameEditText = (EditText) findViewById(R.id.textEditUsername);
-                String username = usernameEditText.getText().toString();
-                preferencesEditor.putString(USERNAME_TAG, username);
+                String teamName = teamNameSpinner.getSelectedItem().toString();
+                preferencesEditor.putString(TEAM_TAG, teamName);
                 preferencesEditor.apply();
             }
         });
