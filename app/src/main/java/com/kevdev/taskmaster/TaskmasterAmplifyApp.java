581e4c1425e62746c190f5ccfc06fd87a36d1a69
KevLaMDev <lamarca.k@gmail.com>
2022-04-12 02:34:02 -0400

    AWS analytics added, text to speech WIP

--- app/src/main/java/com/kevdev/taskmaster/TaskmasterAmplifyApp.java
+++ app/src/main/java/com/kevdev/taskmaster/TaskmasterAmplifyApp.java
@@ -4,9 +4,11 @@ import android.app.Application;
 import android.util.Log;
 
 import com.amplifyframework.AmplifyException;
+import com.amplifyframework.analytics.pinpoint.AWSPinpointAnalyticsPlugin;
 import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
 import com.amplifyframework.core.Amplify;
+import com.amplifyframework.predictions.aws.AWSPredictionsPlugin;
 import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
 
 public class TaskmasterAmplifyApp extends Application
@@ -19,7 +21,9 @@ public class TaskmasterAmplifyApp extends Application
         try {
             Amplify.addPlugin(new AWSApiPlugin());
             Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.addPlugin(new AWSPinpointAnalyticsPlugin(this));
             Amplify.addPlugin(new AWSS3StoragePlugin());
+            Amplify.addPlugin(new AWSPredictionsPlugin());
             Amplify.configure(getApplicationContext());
         } catch (AmplifyException ae) {
             Log.e(TAG, "Error initializing Amplify: " + ae.getMessage(), ae);
